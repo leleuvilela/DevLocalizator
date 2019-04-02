3e8e01eb894be6c69d4e8c5537fdc3c62c68611a
Vinícius Vilela <viniciusvilela19@gmail.com>
2019-04-02 16:48:25 -0300

    att

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/main';
-import Footer from '../components/Footer';
 
 const Routes = () => (
   <BrowserRouter>
